cf5ab2df12ff1b2261f3d2d5f16bde5f44a720a4
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-08 21:39:39 +0330

    feat: implement grid system for palette generator and boilerplate code generator

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,10 @@
-import { Outlet } from "react-router-dom"
-import { Box, Container } from "@chakra-ui/react"
+import { Box, Container, Grid, GridItem } from "@chakra-ui/react"
 import Header from "../components/Header"
 import SideDrawer from "../components/Drawer"
 
 export default function RootLayout() {
 	return (
-		<Box
-			w="100%"
-			h="100%"
-		>
+		<Box w="100%" h="100%">
 			<Header />
 			<SideDrawer />
 			<Container
@@ -18,9 +14,33 @@ export default function RootLayout() {
 					"container.md",
 					"container.lg",
 					"container.xl",
-					"1400px",
+					"1500px",
 				]}
+				h="90dvh"
 			>
+				<Grid
+					templateAreas={[
+						`"palette" "boilerplate"`,
+						null,
+						null,
+						`"palette boilerplate"`,
+					]}
+					gridTemplateRows={["100% 100%", null, null, "100%"]}
+					gridTemplateColumns={["1fr", null, null, "1fr 1fr"]}
+					gap="4"
+					h="100%"
+				>
+					<GridItem
+						area={"palette"}
+						bg={"blackAlpha.300"}
+						borderRadius="md"
+					></GridItem>
+					<GridItem
+						area={"boilerplate"}
+						bg={"blackAlpha.300"}
+						borderRadius="md"
+					></GridItem>
+				</Grid>
 			</Container>
 		</Box>
 	)
