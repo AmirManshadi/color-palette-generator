ee8acee5733f90fbb18259345e2658e4299f6bf1
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-10 21:19:04 +0330

    refactor: redesign palette container and dashboard

--- src/components/PaletteContainer.tsx
+++ src/components/PaletteContainer.tsx
@@ -1,19 +1,31 @@
-import { Box, Grid } from "@chakra-ui/react"
+import { Box, Flex, Grid } from "@chakra-ui/react"
 
 export default function PaletteContainer() {
-	const colors = ["#fff", "#f0f", "#f00", "#0ff", "#00f", "#ff0"]
+	const colors = ["#fff", "#f0f", "#f00", "#0ff", "#00f"]
+
+	const customStyle = {
+		justify: "flex-start",
+		align: "center",
+		p: "5",
+		gap: "5",
+		bg: "blackAlpha.500",
+		my: "10",
+		borderRadius: "md",
+		transition: "background-color 300ms",
+	}
 
 	return (
-		<Grid
-			templateColumns={"1fr 1fr"}
-			templateRows={"repeat(3, 1fr)"}
-			h="100%"
-			gap="3"
-			p="4"
-		>
+		<Flex sx={customStyle} _hover={{ background: "blue" }}>
 			{colors.map(color => (
-				<Box key={color} borderRadius="md" h={"100%"} bg={color} />
+				<Box
+					key={color}
+					borderRadius="md"
+					h={"100%"}
+					bg={color}
+					flexBasis="20%"
+					height="300px"
+				/>
 			))}
-		</Grid>
+		</Flex>
 	)
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,7 @@
-import { Box, Container, Grid, GridItem } from "@chakra-ui/react"
+import { Box, Container } from "@chakra-ui/react"
 import Header from "../components/Header"
 import SideDrawer from "../components/Drawer"
 import PaletteContainer from "../components/PaletteContainer"
-import CodeTabs from "../components/CodeTabs"
 
 export default function RootLayout() {
 	return (
@@ -19,30 +18,7 @@ export default function RootLayout() {
 					"1500px",
 				]}
 			>
-				<Grid
-					templateAreas={[
-						`"palette" "boilerplate"`,
-						null,
-						null,
-						`"boilerplate palette"`,
-					]}
-					gridTemplateRows={["90dvh 90dvh", null, null, "90dvh"]}
-					gridTemplateColumns={["1fr", null, null, "1fr 1fr"]}
-					rowGap="10"
-					columnGap="5"
-					h="100%"
-					alignItems="normal"
-				>
-					<GridItem area={"palette"} bg={"blackAlpha.500"} borderRadius="md">
-						<PaletteContainer />
-					</GridItem>
-					<GridItem
-						area={"boilerplate"}
-						bg={"blackAlpha.500"}
-						borderRadius="md"
-						mb={["5", null, null, "0"]}
-					><CodeTabs /></GridItem>
-				</Grid>
+				<PaletteContainer />
 			</Container>
 		</Box>
 	)
